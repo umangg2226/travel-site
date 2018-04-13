f3fdbfc081c99738c305427866b9d1bed55dbc74
Umang <Umang2226@gmail.com>
2018-04-13 16:51:07 +0530

    complated header mobile

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,23 +1,34 @@
 var gulp = require('gulp'),
-  watch = require('gulp-watch'),
-  browserSync = require('browser-sync').create();
-gulp.task('watch', function() {
-  browserSync.init({
-    notify: false,
-    server: {
-      baseDir: "app"
-    }
-  });
-  watch('./app/index.html', function() {
-    browserSync.reload();
-  });
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
 
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('cssInject');
-  });
+
+gulp.task('watch', function() {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    });
+    watch('./app/index.html', function() {
+        browserSync.reload();
+    });
+
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('cssInject');
+    });
+
+    watch('./app/assets/scripts/**/*.js', function() {
+        gulp.start('scriptsRefresh');
+    })
+
 });
 
 gulp.task('cssInject', ['styles'], function() {
-  return gulp.src('./app/temp/styles/styles.css')
-    .pipe(browserSync.stream());
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
 });
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+    browserSync.reload();
+})
