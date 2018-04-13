6eabcdfa21d16a9cfa2feef733af58173149bf1e
Umang <Umang2226@gmail.com>
2018-04-13 23:46:42 +0530

    complated sticky header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
 import ReavelOnScroll from './modules/ReavelOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 new ReavelOnScroll($(".feature-item"), "85%");
 new ReavelOnScroll($(".testimonial"), "60%");
+var stickyheader = new StickyHeader();
