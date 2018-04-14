2a0bd269f7f9f5029d6947acfe8af5669b5f9e2c
Umang <Umang2226@gmail.com>
2018-04-14 12:03:58 +0530

    complated modal overlay

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,8 +2,10 @@ import MobileMenu from './modules/MobileMenu';
 import ReavelOnScroll from './modules/ReavelOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 new ReavelOnScroll($(".feature-item"), "85%");
 new ReavelOnScroll($(".testimonial"), "60%");
 var stickyheader = new StickyHeader();
+var modal = new Modal();
